e63f5fdb9dcb5f276b4bdeab06d411ac36ffbfdc
umenorin <102865106+umenorin@users.noreply.github.com>
2022-05-10 22:32:45 -0300

    Update Pset2.sql

--- Pset2/Pset2.sql
+++ Pset2/Pset2.sql
@@ -6,40 +6,39 @@ E outra coisa, eu fiz esses SCRIPTs pelo MYsql, caso não rode no PostgreSQL.*\
 
 -- Questão 01
 SELECT numero_departamento, avg(salario)
-FROM funcionario
-GROUP BY numero_departamento;
+ FROM funcionario
+ GROUP BY numero_departamento;
 
 
 -- Questão 02
 SELECT sexo, AVG(salario) AS media_salarial
-FROM funcionario
-WHERE sexo = "M"
-UNION
-SELECT sexo, AVG(salario)
-FROM funcionario
-WHERE sexo = "F";
+ FROM funcionario
+ WHERE sexo = "M"
+ UNION
+ SELECT sexo, AVG(salario)
+ FROM funcionario
+ WHERE sexo = "F";
 
 -- Questão 03
-SELECT  nome_departamento,  primeiro_nome, nome_meio, ultimo_nome,
-data_nascimento, 2022 - year(data_nascimento), salario
-FROM  funcionario, departamento
-WHERE funcionario.numero_departamento = departamento.numero_departamento;
+SELECT  nome_departamento,  primeiro_nome, nome_meio, ultimo_nome, data_nascimento, 2022 - year(data_nascimento), salario
+ FROM  funcionario, departamento
+ WHERE funcionario.numero_departamento = departamento.numero_departamento;
 
 -- Questão 04 
 SELECT primeiro_nome, nome_meio, ultimo_nome, data_nascimento, 2022 -year(funcionario.data_nascimento) AS idade, salario, salario * 1.15 AS 'ajuste_salario'
-FROM funcionario
-WHERE salario >= 35000.00 
-UNION
-SELECT primeiro_nome, nome_meio, ultimo_nome, data_nascimento, 2022 -year(funcionario.data_nascimento) AS idade, salario, salario * 1.2 
-FROM funcionario
-WHERE salario < 35000.00
+ FROM funcionario
+ WHERE salario >= 35000.00 
+ UNION
+ SELECT primeiro_nome, nome_meio, ultimo_nome, data_nascimento, 2022 -year(funcionario.data_nascimento) AS idade, salario, salario * 1.2 
+ FROM funcionario
+ WHERE salario < 35000.00;
 
 -- Questão 05
 SELECT departamento.nome_departamento, f.primeiro_nome as nome_funcionario, funcionario.primeiro_nome as nome_gerente
-FROM departamento as d join funcionario as f on (d.numero_departamento = f.numero_departamento)
-JOIN departamento join funcionario on (departamento.cpf_gerente = funcionario.cpf)
-WHERE departamento.nome_departamento = d.nome_departamento
-ORDER BY d.nome_departamento ASC, f.salario DESC;
+ FROM departamento d join funcionario f on (d.numero_departamento = f.numero_departamento)
+ JOIN departamento join funcionario on (departamento.cpf_gerente = funcionario.cpf)
+ WHERE departamento.nome_departamento = d.nome_departamento
+ ORDER BY d.nome_departamento ASC, f.salario DESC;
 
 -- Questão 06
 SELECT  primeiro_nome, nome_meio, ultimo_nome,
@@ -67,12 +66,9 @@ select concat(f.primeiro_nome,' ',f.nome_meio,' ',f.ultimo_nome) as Nome_complet
             order by departamento;
 
 -- Questão 08
-SELECT nome_departamento, nome_projeto, primeiro_nome, nome_meio, ultimo_nome, horas
-FROM funcionario, departamento, projeto, trabalha_em
-WHERE funcionario.cpf = trabalha_em.cpf_funcionario
-AND trabalha_em.numero_projeto = projeto.numero_projeto
-AND funcionario.numero_departamento = departamento.numero_departamento
-ORDER BY departamento.numero_departamento, nome_projeto, primeiro_nome;
+SELECT primeiro_nome, nome_meio, ultimo_nome, nome_projeto, nome_departamento, horas
+ FROM funcionario f, departamento d, projeto p, trabalha_em t
+ WHERE f.cpf = t.cpf_funcionario AND t.numero_projeto = p.numero_projeto AND f.numero_departamento = d.numero_departamento;
 
 -- Questão 09
 SELECT  p.nome_projeto, d.nome_departamento, SUM(horas)
@@ -82,15 +78,15 @@ SELECT  p.nome_projeto, d.nome_departamento, SUM(horas)
 
 -- Questão 10
 SELECT nome_departamento, avg(salario)
-FROM funcionario AS f, departamento
+FROM funcionario  f, departamento
 GROUP BY nome_departamento
 HAVING avg(salario);
 
 -- Questão 11
 SELECT primeiro_nome, nome_meio, ultimo_nome, p.nome_projeto, horas * 50 as horas_trabalhadas
-FROM funcionario join projeto as p join trabalha_em as t on (p.numero_projeto = t.numero_projeto)
-WHERE funcionario.cpf = t.cpf_funcionario 
-ORDER BY funcionario.primeiro_nome;
+ FROM funcionario join projeto  p join trabalha_em  t on (p.numero_projeto = t.numero_projeto)
+ WHERE funcionario.cpf = t.cpf_funcionario 
+ ORDER BY funcionario.primeiro_nome;
 
 -- Questão 12
 SELECT  nome_departamento, nome_projeto, primeiro_nome
@@ -106,13 +102,17 @@ ORDER BY idade desc;
 
 -- Questão 14
 SELECT f.numero_departamento, d.nome_departamento, COUNT(*) AS total_por_departamento 
-FROM funcionario AS f 
-JOIN departamento AS d ON (f.numero_departamento = d.numero_departamento)
+FROM funcionario f 
+JOIN departamento d ON (f.numero_departamento = d.numero_departamento)
 GROUP BY numero_departamento;
 
 -- Questão 15
-SELECT f.primeiro_nome, f.nome_meio, f.ultimo_nome, d.nome_departamento, p.nome_projeto
-FROM funcionario AS f 
-INNER JOIN departamento d ON (d.cpf_gerente = f.cpf)
-INNER JOIN trabalha_em t ON (t.cpf_funcionario = f.cpf)
-INNER JOINn projeto p ON (p.numero_projeto = t.numero_projeto);
+SELECT primeiro_nome, nome_meio, ultimo_nome, d.nome_departamento, p.nome_projeto
+ FROM trabalha_em  t LEFT JOIN projeto p on (p.numero_projeto = t.numero_projeto)
+ JOIN funcionario  f JOIN departamento d on (d.numero_departamento = f.numero_departamento)
+ WHERE cpf_funcionario = f.cpf;
+
+ 
+ 
+ 
+ 
