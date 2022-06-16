b1696d3bd30d7dfa029dab0ff1c66fe436d887c8
umenorin <102865106+umenorin@users.noreply.github.com>
2022-06-16 13:46:01 -0300

    Update pset3.sql

--- PSET 3/pset3.sql
+++ PSET 3/pset3.sql
@@ -1,12 +1,12 @@
 /* o script pode ser feito dessas 2 maneiras*\
-with recursive part
+with recursive pset
 as(select c.nome
 from classificacao  c
 where codigo_pai is null
 union ALL
 select c.nome
 from classificacao c
-inner join part on codigo = codigo_pai
+inner join pset on codigo = codigo_pai
 where codigo_pai is not null)
 select codigo, codigo_pai, nome
 from classificacao c 
