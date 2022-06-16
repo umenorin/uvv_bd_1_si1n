021ed8e142f701567f3af58404272883ad74f617
umenorin <102865106+umenorin@users.noreply.github.com>
2022-06-16 13:33:27 -0300

    Add files via upload

---
+++ PSET 3/pset3.sql
@@ -0,0 +1,18 @@
+/* o script pode ser feito dessas 2 maneiras*\
+with recursive part
+as(select c.nome
+from classificacao  c
+where codigo_pai is null
+union ALL
+select c.nome
+from classificacao c
+inner join part on codigo = codigo_pai
+where codigo_pai is not null)
+select codigo, codigo_pai, nome
+from classificacao c 
+order by codigo asc, codigo_pai asc;
+
+
+select codigo, codigo_pai, nome
+from classificacao c ;
+order by codigo asc, codigo_pai asc;
